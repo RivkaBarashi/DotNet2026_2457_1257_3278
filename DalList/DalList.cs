f8da3f0ea7deda3f17dfd344f5e9c13c97c2706c
RachelSheinfeld <r0583226902@gmail.com>
2026-02-24 13:26:55 +0200

    part7

---
+++ DalList/DalList.cs
@@ -0,0 +1,22 @@
+﻿using DO;
+
+using DalApi;
+using DalFacade.DalApi;
+using System.ComponentModel;
+
+namespace Dal
+{
+    internal sealed class DalList : IDal
+    {
+        public ICustomer Customer => new CustomerImplementation();
+        public IProduct Product => new ProductImplementation();
+        public ISale Sale => new SaleImplementation();
+        private  readonly DalList instance=new DalList();
+        public DalList Instance { get { return instance; } }
+
+        private DalList()
+        {
+
+        }
+    }
+}
---
+++ DalList/DataSource.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Dal;
+using DO;
+
+
+internal static class DataSource
+{
+    internal static List<Customer?> Customers = new List<Customer?>();
+    internal static List<Product?> Products = new List<Product?>();
+    internal static List<Sale?> Sales = new List<Sale?>();
+    internal static class Config
+    {
+        internal const int firstIdProduct = 100000;
+        private static int productId = firstIdProduct;
+        public static int GetProductId()
+        {
+            return productId++;
+        }
+
+        internal const int firstIdSale = 100;
+        private static int saleId = firstIdSale;
+        public static int GetSaleId()
+        {
+            return saleId++;
+        }
+    }
+
+
+}
